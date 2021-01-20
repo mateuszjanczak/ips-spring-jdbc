ad0ae15703fae78eb1e4ca80520eaabcfc5797d9
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-01-20 20:23:17 +0100

    Initial commit & dependencies

---
+++ src/test/java/com/mateuszjanczak/ips/IpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszjanczak.ips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
